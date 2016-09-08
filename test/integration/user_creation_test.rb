8eb7584827633d0c25c4686535aa4d6d1d2aba45
David Davydov <kctrlv@gmail.com>
2016-09-08 17:49:03 -0600

    visitor_can_create_user_account

---
+++ test/integration/user_creation_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UserCreationTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  test "visitor can create user account" do
+    visit new_user_path
+    fill_in "Username", with: "davidos"
+    fill_in "Password", with: "password"
+    click_button "Create Account"
+
+    assert page.has_content?("Welcome, davidos!")
+  end
+end
