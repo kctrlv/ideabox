300919ed302f7b4d3a4fa378ffa0d4070dcf7734
David Davydov <kctrlv@gmail.com>
2016-09-11 12:18:15 -0600

    user can logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,12 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-
+      render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
---
+++ test/integration/user_logout_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserLogoutTest < ActionDispatch::IntegrationTest
+  test "logged in user can logout" do
+    user = User.create(username: "dave", password: "pass")
+
+    visit login_path
+    fill_in "Username", with: "dave"
+    fill_in "Password", with: "pass"
+    click_button "Login"
+
+    click_link "Logout"
+    save_and_open_page
+    refute page.has_content?("Signed in as: dave")
+  end
+end
